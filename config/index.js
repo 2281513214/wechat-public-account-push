52701156d579c6cc45403eb2127a8fa4cbd17f26
2281513214 <113603829+2281513214@users.noreply.github.com>
2022-09-15 20:35:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wxc29191cf40e7ac19",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "11f61afca37a13f48e074e1e80a295c7",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -20,12 +20,12 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [
+  USERS: [KJc7d2sL1azOoJ0z3aevOCcS5qiF8pcdIHpY4FEKhrY
     {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oZdig6sIEwwgfqKfU_wdhGPZuzds",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "",
       // 所在省份
